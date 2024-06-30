0b3fbd26998fcd549fc475d83c708fc3077cfb60
PC <PC@doan-the-phong>
2024-06-30 23:00:25 +0700

    sửa phong code

--- src/all/NewClass.java
+++ src/all/NewClass.java
@@ -13,6 +13,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("vai");
         System.out.println("vc");
-
+        System.out.println("phong code...");
     }
 }
